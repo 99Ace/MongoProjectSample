19f78404d2eae4a21528e5af21ac94a6b1299161
Ubuntu <ubuntu@ip-172-31-88-147.ec2.internal>
2019-10-24 09:51:42 +0000

    created link to index.html and passing of data over

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 	
 # Setting up the MONGODB DATABASES to be link up	
 MONGO_URI = os.getenv('MONGO_URI')
-DATABASE_NAME = 'cars_listing'
-COLLECTION_NAME = 'car_plate'
+DATABASE_NAME = 'insurance'
+COLLECTION_NAME = 'policyholder'
 
 # set up the connection to MONGO_URI which we set up in bashrc
 conn = pymongo.MongoClient(MONGO_URI)
@@ -18,10 +18,13 @@ data = conn[DATABASE_NAME][COLLECTION_NAME]
 
 @app.route('/') # map the root route to the index function
 def index():
-    return "hello world" # Testing to make sure the route is working, run 'python3 app.py' on terminal 
-                         # to test for 'hello world' to be displayed 
+    result = data.find({})
+    return render_template ("index.html" , data = result)
     
-
+@app.route('/viewlist')
+def viewlist():
+    policyowner = data.find({})
+    return render_template ("viewlist.html", data = policyowner)
     
 # "magic code" -- boilerplate
 if __name__ == '__main__':
